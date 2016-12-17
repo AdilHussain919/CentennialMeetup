52a534bafe51c9ee0d61378fdb9b61ec8271eed3
Viranch Shah <vir.shah1991@gmail.com>
2016-12-17 06:29:20 -0500

    [Sagar & Viranch] Home module

--- app/src/main/java/com/meetup/centennial/centennialmeetup/HomeActivity.java
+++ app/src/main/java/com/meetup/centennial/centennialmeetup/HomeActivity.java
@@ -3,6 +3,7 @@ package com.meetup.centennial.centennialmeetup;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,11 +15,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,6 +40,7 @@ public class HomeActivity extends AppCompatActivity
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
+   public  String Userid;
     private GoogleApiClient client;
 
     @Override
@@ -35,13 +49,17 @@ public class HomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent t =getIntent();
+
+         Userid = t.getExtras().getString("UserId");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent searchIntent = new Intent(getApplicationContext(), SearchActivity.class);
+                searchIntent.putExtra("Uid",Userid);
+                startActivity(searchIntent);
             }
         });
 
@@ -56,6 +74,54 @@ public class HomeActivity extends AppCompatActivity
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        JSONObject json = null;
+        try{
+            String url = "http://10.0.2.2:8000/centennialmeetup/getprofile?centennialid=" + Userid;
+            URL getprofileAPI = new URL(url);
+            HttpURLConnection client = null;
+            client = (HttpURLConnection) getprofileAPI.openConnection();
+            client.setRequestMethod("GET");
+            client.setDoOutput(true);
+            client.setDoInput(true);
+            client.connect();
+
+            StringBuilder sb = new StringBuilder();
+            int HttpResult = client.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(client.getInputStream(), "utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                    json = new JSONObject(sb.toString());
+                    System.out.println(json);
+                }
+                br.close();
+            }
+
+            navigationView.setNavigationItemSelectedListener(this);
+            View header=navigationView.getHeaderView(0);
+
+            TextView fname = (TextView) header.findViewById(R.id.UsernameTV);
+            fname.setText(json.getString("firstname"));
+            TextView campus = (TextView) header.findViewById(R.id.UserCampusTV);
+            campus.setText(json.getString("campus"));
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -78,13 +144,17 @@ public class HomeActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
         } else if (id == R.id.nav_profile) {
             Intent profileIntent = new Intent(this, ProfileView.class);
+            profileIntent.putExtra("Uid",Userid);
             startActivity(profileIntent);
 
         } else if (id == R.id.nav_friends) {
 
+
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_logout) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
 
         } else if (id == R.id.nav_aboutUs) {
 
