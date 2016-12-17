cfb825942153f5ccaddd98d55b1ac0d1d6339b72
Viranch Shah <vir.shah1991@gmail.com>
2016-12-17 06:26:18 -0500

    [Jasvant & Zeal] Profile module

--- app/src/main/java/com/meetup/centennial/centennialmeetup/EditProfile.java
+++ app/src/main/java/com/meetup/centennial/centennialmeetup/EditProfile.java
@@ -1,11 +1,24 @@
 package com.meetup.centennial.centennialmeetup;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditProfile extends AppCompatActivity {
 
+    private TextView lable ;
+    private EditText value;
+    private Button update;
+    DatabaseManager db ;
+    ProfileView obj ;
+  public  static ContentValues values;
+    public static  String heading,editValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +28,85 @@ public class EditProfile extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
-        getWindow().setLayout((int)(width*.4),(int)(height*.4));
+        getWindow().setLayout((int)(width*.8),(int)(height*.4));
+
+       heading = getIntent().getStringExtra("texthead");
+        editValue = getIntent().getStringExtra("value");
+
+        lable = (TextView)findViewById(R.id.heading);
+        value = (EditText)findViewById(R.id.value);
+        update = (Button)findViewById(R.id.but_update);
+        lable.setText(heading);
+        value.setText(editValue);
+
+
+
+
+
+
+            update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            // String updated_value =   value.getText().toString();
+
+
+                //adding into database
+              //  values.put("centennial_id",updated_value);
+                if(heading.equals("First Name"))
+                {
+
+                    db.updateRecord(values,"User","firstname",value.getText().toString());
+                }
+                else if(heading.equals("Last Name"))
+                {
+                    db.updateRecord(values,"User","lastname",value.getText().toString());
+
+                }
+                else if(heading.equals("Email"))
+                {
+                    db.updateRecord(values,"User","emailid",value.getText().toString());
+
+                }
+                else if(heading.equals("Date Of Birth"))
+                {
+                    db.updateRecord(values,"User","dateofbirth",value.getText().toString());
+
+                }
+
+
+                else if(heading.equals("Hobby"))
+                {
+                    db.updateRecord(values,"User","hobby",value.getText().toString());
+
+                }
+                else if(heading.equals("Position"))
+                {
+                    db.updateRecord(values,"User","position",value.getText().toString());
+
+                }
+                else if(heading.equals("Program"))
+                {
+                    db.updateRecord(values,"User","program",value.getText().toString());
+
+                }
+                else if(heading.equals("department"))
+                {
+                    db.updateRecord(values,"User","department",value.getText().toString());
+
+                }
+                else if(heading.equals("Campus"))
+                {
+                    db.updateRecord(values,"User","campus",value.getText().toString());
+
+                }
+
+                else
+                {
+                    Toast.makeText(EditProfile.this,"Invalid Data",Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/meetup/centennial/centennialmeetup/ProfileView.java
+++ app/src/main/java/com/meetup/centennial/centennialmeetup/ProfileView.java
@@ -5,106 +5,285 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import static com.meetup.centennial.centennialmeetup.R.id.add;
+import static com.meetup.centennial.centennialmeetup.R.id.edit_fname;
 
 public class ProfileView extends AppCompatActivity {
     SQLiteDatabase dba;
 
-    private static final String tables[]={"tbl_users","tbl_friends"};
-    //
-    private static final String tableCreatorString[] ={"CREATE TABLE tbl_users (user_id INTEGER PRIMARY KEY AUTOINCREMENT ," +
-            "centennial_id TEXT, firstname TEXT, lastname TEXT, address TEXT, email TEXT, DOB TEXT, hobby TEXT, program TEXT, origin TEXT, campus TEXT);",
-            "CREATE TABLE tbl_friends (user_id INTEGER, friend_id INTEGER);"};
-    public static TextView centId,fname,lname,email,address,hobby,dob,program,campus,origin,editPro;
+
+    public String Uid;
+
+    public static TextView centId,fname,lname,pos,email,hobby,dob,program,campus,department,editfname,editlname,editaddress,editemail,editdob,edithobby,editprog,editorigin,editcampus;
+
+    Button btnAddFriend, btnSendText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+        Intent t = getIntent();
 
-        centId = (TextView)findViewById(R.id.tv_id);
-        fname  = (TextView)findViewById(R.id.tv_fn);
-        lname = (TextView)findViewById(R.id.tv_ln);
-        address  = (TextView)findViewById(R.id.tv_add);
-        email = (TextView)findViewById(R.id.tv_email);
-        dob  = (TextView)findViewById(R.id.tv_dob);
-        hobby = (TextView)findViewById(R.id.tv_hobby);
-        program  = (TextView)findViewById(R.id.tv_program);
-        campus = (TextView)findViewById(R.id.tv_campus);
-        origin = (TextView)findViewById(R.id.tv_origin) ;
-        editPro = (TextView)findViewById(R.id.edit_fname);
-        dba=openOrCreateDatabase("Clinic.db", SQLiteDatabase.CREATE_IF_NECESSARY,null);
-        final DatabaseManager db = new DatabaseManager(this);
-        //db.createDatabase(getApplicationContext());
-        db.dbInitialize( tables,tableCreatorString);
-        final String fields[] = {"centennial_id","firstname","lastname","address","email","dob","hobby","program","origin","campus"};
-        final String record[] = new String[10];
-        //Edit Text Fields
-        //Add references to edit text fields bellow
+        Uid = t.getExtras().getString("Uid");
 
-        // Handle Save button
-        // Add reference to add User button here
-//retrieving info from edit text fields and putting into record array
-        record[0]="User1";
-        record[1]="amnsdns";
-        record[2]="sdsadadas";
-        record[3]="dsadasdsa";
-        record[4]="sdsadsads";
-        record[5]="sdsadsads";
-        record[6]="sdsadsd";
-        record[7]="sdsadsadsa";
-        record[8]="sdsadsdsd";
-        record[9]="dsadsadsadd";
-        //checking if added
-        Log.d("First Name: ",record[0]);
-        ContentValues values=new ContentValues();
-        //adding into database
-        values.put("centennial_id",record[0]);
-        values.put("firstname",record[1]);
-        values.put("lastname",record[2]);
-        values.put("address",record[3]);
-        values.put("email",record[4]);
-        values.put("dob",record[5]);
-        values.put("hobby",record[6]);
-        values.put("program",record[7]);
-        values.put("origin",record[8]);
-        values.put("campus",record[9]);
-        db.addRecord(values, "tbl_users",fields,record);
 
-        //dba.insert("tbl_users",null,values);
-        //dba.close();
-        //    db.getStudentById(1);
-        Cursor cursor = db.getUser("tbl_users","User1");
+        centId = (TextView) findViewById(R.id.tv_id);
+        fname = (TextView) findViewById(R.id.tv_fn);
+        lname = (TextView) findViewById(R.id.tv_ln);
+        email = (TextView) findViewById(R.id.tv_email);
+        dob = (TextView) findViewById(R.id.tv_dob);
+        hobby = (TextView) findViewById(R.id.tv_hobby);
+        pos = (TextView) findViewById(R.id.tv_pos);
+        program = (TextView) findViewById(R.id.tv_program);
+        campus = (TextView) findViewById(R.id.tv_campus);
+        department = (TextView) findViewById(R.id.tv_dept);
+        editfname = (TextView) findViewById(R.id.edit_fname);
+        editlname = (TextView) findViewById(R.id.edit_lname);
+        editaddress = (TextView) findViewById(R.id.edit_address);
+        editemail = (TextView) findViewById(R.id.edit_email);
+        editdob = (TextView) findViewById(R.id.edit_dob);
+        editprog = (TextView) findViewById(R.id.edit_program);
+        editcampus = (TextView) findViewById(R.id.edit_campus);
+        editorigin = (TextView) findViewById(R.id.edit_origin);
+        edithobby = (TextView) findViewById(R.id.edit_hobby);
+
+        ////////////////---------------- Viranch ----------------////////////////////
+        /*final DatabaseManager db = new DatabaseManager(this);
+
+        Cursor cursor = db.getUser("User","300853316");
         cursor.moveToFirst();
-        centId.setText(cursor.getString(1));
-        fname.setText(cursor.getString(2));
-        lname.setText(cursor.getString(3));
-        address.setText(cursor.getString(4));
-        email.setText(cursor.getString(5));
-        dob.setText(cursor.getString(6));
-       hobby.setText(cursor.getString(7));
-        program.setText(cursor.getString(8));
-        origin.setText(cursor.getString(9));
-        campus.setText(cursor.getString(10));
+
+        centId.setText(cursor.getString(0));
+        fname.setText(cursor.getString(1));
+        lname.setText(cursor.getString(2));
+        email.setText(cursor.getString(3));
+        dob.setText(cursor.getString(4));
+        hobby.setText(cursor.getString(5));
+        String pas = cursor.getString(6);
+        program.setText(cursor.getString(7));
+        department.setText(cursor.getString(8));
+        campus.setText(cursor.getString(9));
+        pos.setText(cursor.getString(10));
 
         if(!cursor.isClosed())
         {
             cursor.close();
+        }*/
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        JSONObject json = null;
+        try {
+            String url = "http://10.0.2.2:8000/centennialmeetup/getprofile?centennialid=" + Uid;
+            URL getprofileAPI = new URL(url);
+            HttpURLConnection client = null;
+            client = (HttpURLConnection) getprofileAPI.openConnection();
+            client.setRequestMethod("GET");
+            client.setDoOutput(true);
+            client.setDoInput(true);
+            client.connect();
+
+            StringBuilder sb = new StringBuilder();
+            int HttpResult = client.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(client.getInputStream(), "utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                    json = new JSONObject(sb.toString());
+                    System.out.println(json);
+                }
+                br.close();
+            }
+
+            centId.setText(json.getString("centennialid"));
+            fname.setText(json.getString("firstname"));
+            lname.setText(json.getString("lastname"));
+            email.setText(json.getString("email"));
+            dob.setText(json.getString("dateofbirth"));
+            hobby.setText(json.getString("hobby"));
+            program.setText(json.getString("program"));
+            department.setText(json.getString("department"));
+            campus.setText(json.getString("campus"));
+            pos.setText(json.getString("profession"));
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-        editPro.setOnClickListener(new View.OnClickListener() {
+        ////////////////////////----------------------------------////////////////////////////
+
+        editfname.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (ProfileView.this,EditProfile.class);
+
+                String head = "First Name";
+                String value = fname.getText().toString();
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
                 startActivity(intent);
 
             }
         });
+        editlname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Last Name";
+                String value = lname.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+            }
+        });
+        editaddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Email";
+                String value = email.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+        editemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Date Of Birth";
+                String value = dob.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+        editdob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Hobby";
+                String value = hobby.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+        edithobby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String head = "Position";
+                String value = pos.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+        editprog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Program";
+                String value = program.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+        editorigin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Department";
+                String value = department.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+        editcampus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String head = "Campus";
+                String value = campus.getText().toString();
+
+                Intent intent = new Intent(ProfileView.this, EditProfile.class);
+                intent.putExtra("texthead", head);
+                intent.putExtra("value", value);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+/*
+//        btnAddFriend.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //dba.addFriend(intUserId,current_user_id);
+
+            }
+        });
+*/
+
+        public void sendAText(View view){
+                Intent intent = new Intent(ProfileView.this,SendSMSActivity.class);
+                intent.putExtra("phonenum","5556");
+                startActivity(intent);
+        };
 
 
         //Check UpdateRecord Method in database manager and create onClick method for Updating and Saving profile
         //Code for updating is similar to the code provided above    // }
     }
-}
